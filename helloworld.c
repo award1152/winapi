16e70a49688a55d6fc65d8a0de8040ff6ef7a04e
award1152 <award1152@gmail.com>
2023-12-18 23:08:21 -0500

    main

--- helloworld.c
+++ helloworld.c
@@ -1,1 +1,12 @@
+//Simple HelloWorld with WindowsAPI
 
+#include <windows.h>
+
+
+
+
+void main(){
+    const char * message = "Hello World";
+    const char * title = "My First Windows API usage";
+    MessageBox(NULL, message, title, MB_OK);
+}
